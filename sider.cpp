f62ebefdbb942db04ee4776915b6ef3eeac7c093
Anton Jouline <juce66@gmail.com>
2019-02-16 20:26:03 -0800

    using linear filter for texture sampling for better visual results

--- sider.cpp
+++ sider.cpp
@@ -2092,7 +2092,7 @@ void prep_stuff()
 
     // Create the state objects
     D3D11_SAMPLER_DESC sampDesc = {};
-    sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+    sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
     sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
     sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
     sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;

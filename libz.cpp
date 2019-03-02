e0fb6e1661ec201a085d1bb82e2ecf51c38181ef
Anton Jouline <juce66@gmail.com>
2019-03-02 00:42:05 -0800

    correction for lua stack handling in zlib library

--- libz.cpp
+++ libz.cpp
@@ -16,7 +16,6 @@ static int libz_compress(lua_State *L)
         lua_pushstring(L, "data cannot be of zero-length");
         return 2;
     }
-    lua_pop(L, 1);
 
     uLongf dest_len;
     BYTE* dest;
@@ -25,12 +24,14 @@ static int libz_compress(lua_State *L)
     dest = (BYTE*)malloc(dest_len); // big buffer just in case;
     int retval = compress(dest, &dest_len, (const Bytef*)data, len);
     if (retval != Z_OK) {
+        lua_pop(L, 1);
         lua_pushnil(L);
         lua_pushfstring(L, "compression failed with error code: %d", retval);
         free(dest);
         return 2;
     }
 
+    lua_pop(L, 1);
     lua_pushlstring(L, (char*)dest, dest_len);
     free(dest);
     return 1;
@@ -41,16 +42,15 @@ static int libz_uncompress(lua_State *L)
     size_t len = 0;
     int uncompressed_len = 0;
     const char *data = luaL_checklstring(L, 1, &len);
-    if (lua_isnumber(L, 2)) {
+    if (lua_gettop(L) > 1) {
         uncompressed_len = luaL_checkint(L, 2);
     }
     if (!data || len == 0) {
-        lua_pop(L, 1);
+        lua_pop(L, lua_gettop(L));
         lua_pushnil(L);
         lua_pushstring(L, "compressed data cannot be of zero-length");
         return 2;
     }
-    lua_pop(L, 1);
 
     uLongf dest_len;
     BYTE* dest;
@@ -59,12 +59,14 @@ static int libz_uncompress(lua_State *L)
     dest = (BYTE*)malloc(dest_len);
     int retval = uncompress(dest, &dest_len, (const Bytef*)data, len);
     if (retval != Z_OK) {
+        lua_pop(L, lua_gettop(L));
         lua_pushnil(L);
         lua_pushfstring(L, "decompression failed with error code: %d", retval);
         free(dest);
         return 2;
     }
 
+    lua_pop(L, lua_gettop(L));
     lua_pushlstring(L, (char*)dest, dest_len);
     free(dest);
     return 1;
--- modules/zlibtest.lua
+++ modules/zlibtest.lua
@@ -9,6 +9,14 @@ function m.init(ctx)
     local uncompressed = zlib.uncompress(compressed)
     log(string.format("uncompressed: %s", uncompressed))
     assert(data == uncompressed)
+
+    -- test: bogus compressed data
+    local result,err = zlib.uncompress("plaintext")
+    log(string.format("expected problem:: result: %s, error: %s", result, err))
+
+    -- test: uncompressed buffer too small
+    local result,err = zlib.uncompress(compressed, 4)
+    log(string.format("expected problem:: result: %s, error: %s", result, err))
 end
 
 return m

0eb141255dd3bd0add6a35b134c7af4c219bfda2
Anton Jouline <juce66@gmail.com>
2019-03-03 03:17:11 -0800

    adding test module for GC64

---
+++ modules/gc64test.lua
@@ -0,0 +1,24 @@
+-- testing large allocation
+
+local m = {}
+
+local t = {}
+
+function m.init(ctx)
+    local num_bytes = 1024*1024*1024
+    local ctype = string.format("char[%s]", num_bytes)
+    for i=1,8 do
+        local buf = ffi.new(ctype)
+        ffi.fill(buf, num_bytes, 0x40+i)
+        local chunk = ffi.string(buf, num_bytes)
+        collectgarbage("collect")
+        log(string.format("successfully allocated chunk %s of size: %s", i, #chunk))
+        t[#t+1] = chunk
+    end
+    log(string.format("Lua memory usage now: %0.3f KB", collectgarbage("count")))
+    log(t[1]:sub(1,10))
+    log(t[2]:sub(1,10))
+    log(t[3]:sub(1,10))
+end
+
+return m
